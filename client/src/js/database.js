41ce2d86e86be00442688460b97674c45c61d3f0
Nevah <165732847+nevah-evans@users.noreply.github.com>
2024-08-25 17:54:38 -0500

    adding database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {console.error('putDb not implemented');
+console.log('PUT to the database');
+const textDb = await openDB('text', 1);
+const tx = textDb.transaction('text', 'readwrite');
+const store = tx.objectStore('text');
+const request = store.add({ text: content });
+const result = await request;
+console.log('Data saved to the database', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.error('getDb not implemented');
+console.log('GET all from the database');
+const textDb = await openDB('text', 1);
+const tx = textDb.transaction('text', 'readonly');
+const store = tx.objectStore('text');
+const request = store.getAll();
+const result = await request;
+console.log('result.value', result);
+return result;
+};
 
 initdb();
